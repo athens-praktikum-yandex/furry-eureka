ac31f770a21918468e0d40be677d3c164c82d2f1
Timofey Ponomarev <hiderrweb@yandex.ru>
2021-03-17 17:55:04 +0300

    Remove babel finally

--- webpack.config.js
+++ webpack.config.js
@@ -18,11 +18,6 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
       }
