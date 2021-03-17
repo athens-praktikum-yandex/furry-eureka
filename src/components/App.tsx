1dea08f91d90b5752e0f0dcf40de92fe03c2b81d
spotsccc <chernenko.ivan,17k@gmail.com>
2021-03-17 19:28:11 +0300

    fix spacing before and after brackets and use space instead of tabs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
 export const App = () => <div>
-    <h1>Мой апп.</h1>
-</div>
+  <h1>Мой апп.</h1>
+</div>;
