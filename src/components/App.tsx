df0b68719cd5f6d4f816d81127de752c63df685c
Ivan Chernenko <chernenko.ivan17@gmail.com>
2021-03-17 18:33:54 +0300

    change App component from class to function and use named export instead of default

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Мой апп.</h1>
-      </div>
-    );
-  }
-}
-export default App;
+export const App = () => <div>
+    <h1>Мой апп.</h1>
+</div>
