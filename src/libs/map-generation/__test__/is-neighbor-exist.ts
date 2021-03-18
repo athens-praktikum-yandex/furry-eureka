4526b50dc94637d3c063f66661f96a365b7ab86f
spotsccc <chernenko.ivan.k17@gmail.com>
2021-03-18 15:11:01 +0300

    add types to isNeighbor signature

--- src/libs/map-generation/__test__/is-neighbor-exist.ts
+++ src/libs/map-generation/__test__/is-neighbor-exist.ts
@@ -12,7 +12,7 @@ const isTopNeighbor = (cell1: Cell, cell2: Cell) => cell1.y === cell2.y - 1
 const isBottomNeighbor = (cell1: Cell, cell2: Cell) => cell1.y === cell2.y + 1
   && cell1.x === cell2.x;
 
-const isNeighbor = (cell1, cell2) => isLeftNeighbor(cell1, cell2)
+const isNeighbor = (cell1: Cell, cell2: Cell) => isLeftNeighbor(cell1, cell2)
   || isRightNeighbor(cell1, cell2)
   || isTopNeighbor(cell1, cell2)
   || isBottomNeighbor(cell1, cell2);
