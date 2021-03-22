dba4ade45a63360809065e366bc9ab619218d0bd
VyacheslavR <ryutin.vyacheslav@gmail.com>
2021-03-22 08:36:19 +0800

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,21 @@
 module.exports = {
-  extends: ['airbnb-typescript/base'],
+  extends: ['airbnb-typescript'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'eslint.tsconfig.json',
+    project: './tsconfig.json',
   },
   env: {
     browser: true,
     node: true
   },
+  ignorePatterns: '*.js',
   plugins: [
     'prettier'
   ],
   rules: {
-    'linebreak-style': 0,
-    'no-trailing-spaces': 0,
-    '@typescript-eslint/space-infix-ops': 0,
-    '@typescript-eslint/object-curly-spacing': 0,
     'import/prefer-default-export': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/ban-types': 1,
     'class-methods-use-this': 0,
-    'import/extensions': 0,
     'no-underscore-dangle': 0
   }
 };
--- src/libs/map-generation/__test__/is-neighbor-exist.ts
+++ src/libs/map-generation/__test__/is-neighbor-exist.ts
@@ -1,21 +1,24 @@
 import { Cell } from '../types';
 
-const isLeftNeighbor = (cell1: Cell, cell2: Cell) => cell1.y === cell2.y
-  && cell1.x === cell2.x - 1;
+const isLeftNeighbor = (cell1: Cell, cell2: Cell) => (
+  cell1.y === cell2.y && cell1.x === cell2.x - 1
+);
 
-const isRightNeighbor = (cell1: Cell, cell2: Cell) => cell1.y === cell2.y
-  && cell1.x === cell2.x + 1;
+const isRightNeighbor = (cell1: Cell, cell2: Cell) => (
+  cell1.y === cell2.y && cell1.x === cell2.x + 1
+);
 
-const isTopNeighbor = (cell1: Cell, cell2: Cell) => cell1.y === cell2.y - 1
-  && cell1.x === cell2.x;
+const isTopNeighbor = (cell1: Cell, cell2: Cell) => (
+  cell1.y === cell2.y - 1 && cell1.x === cell2.x
+);
 
-const isBottomNeighbor = (cell1: Cell, cell2: Cell) => cell1.y === cell2.y + 1
-  && cell1.x === cell2.x;
+const isBottomNeighbor = (cell1: Cell, cell2: Cell) => (
+  cell1.y === cell2.y + 1 && cell1.x === cell2.x
+);
 
 const isNeighbor = (cell1: Cell, cell2: Cell) => isLeftNeighbor(cell1, cell2)
   || isRightNeighbor(cell1, cell2)
   || isTopNeighbor(cell1, cell2)
   || isBottomNeighbor(cell1, cell2);
 
-export const isNeighborExist = (cell: Cell, map: Cell[]) => map
-  .some((c) => isNeighbor(cell, c));
+export const isNeighborExist = (cell: Cell, map: Cell[]) => map.some((c) => isNeighbor(cell, c));
