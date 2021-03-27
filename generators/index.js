377b23a9710e1d5e7eb7d55570c72cf2dcb251bc
VyacheslavR <ryutin.vyacheslav@gmail.com>
2021-03-27 17:58:10 +0800

    add feature in generators

--- generators/index.js
+++ generators/index.js
@@ -1,41 +1,9 @@
+const getGeneratorOptions = require("./utils/getGeneratorOptions");
+
 module.exports = plop => {
-  plop.setGenerator('component', {
-    prompts: [{
-      type: 'input',
-      name: 'name',
-      message: 'Component name',
-    }],
-    actions: [
-      {
-        type: 'add',
-        path: '../src/components/{{ name }}/{{ name }}.css',
-        templateFile: 'templates/styles.hbs',
-        abortOnFail: true,
-      },
-      {
-        type: 'add',
-        path: `../src/components/{{ name }}/{{ name }}.stories.tsx`,
-        templateFile: 'templates/stories.hbs',
-        abortOnFail: true,
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{ name }}/{{ name }}.tsx',
-        templateFile: 'templates/component.hbs',
-        abortOnFail: true,
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{ name }}/types.ts',
-        templateFile: 'templates/types.hbs',
-        abortOnFail: true,
-      },
-      {
-        type: 'add',
-        path: '../src/components/{{ name }}/index.ts',
-        templateFile: 'templates/index.hbs',
-        abortOnFail: true,
-      },
-    ],
-  });
+  const componentOptions = getGeneratorOptions('component', '../src', './templates');
+  const featuresOptions = getGeneratorOptions('feature', '../src', './templates');
+
+  plop.setGenerator('component', componentOptions);
+  plop.setGenerator('feature', featuresOptions);
 };
