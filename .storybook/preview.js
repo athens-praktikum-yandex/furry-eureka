4c1cf1d83db84b7e141510cbcadf0b299b3caf08
VyacheslavR <ryutin.vyacheslav@gmail.com>
2021-03-27 15:50:10 +0800

    update storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import { addDecorator } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
 import { jsxDecorator } from 'storybook-addon-jsx';
 
 import '../src/assets/styles/main.css';
 import '../www/assets/fontello/css/fontello.css';
 
 addDecorator((storyFn) => (
-  <div style={{
-    width: '100%',
-    height: '100vh',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }}>
-    {storyFn()}
-  </div>
+  <MemoryRouter initialEntries={['/']}>
+    <div style={{
+      width: '100%',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      {storyFn()}
+    </div>
+  </MemoryRouter>
 ));
 
-addDecorator(withKnobs);
 addDecorator(jsxDecorator);
+
+
