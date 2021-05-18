bb4fecafe775bdcd266c4ae5c49973178ab47dae
Timofey Ponomarev <hiderrweb@yandex.ru>
2021-05-18 10:47:20 +0300

    Fixed Docker issue

--- api/app/lib/sequelize.ts
+++ api/app/lib/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 
 export const sequelize = new Sequelize({
-  host: 'localhost',
+  host: 'postgres',
   port: 5432,
   username: 'postgres',
   password: 'newPassword',
