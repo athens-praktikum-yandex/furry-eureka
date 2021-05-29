9528b7b31087abadcc6beb2c3aa98ebc098d83fa
Vyacheslav Ryutin <ryutin.vyacheslav@gmail.com>
2021-05-29 21:53:28 +0800

    useForum: fix useSelectors

--- src/features/Forum/hooks/useForum.ts
+++ src/features/Forum/hooks/useForum.ts
@@ -14,11 +14,11 @@ import { createUser } from '../store/actions';
 export const useForum = () => {
   const dispatch = useDispatch();
 
-  const topics = useSelector<State, TopicType[]>((state) => forumSelectors.getTopics(state));
-  const comments = useSelector<State, Comment[]>((state) => forumSelectors.getComments(state));
-  const replies = useSelector<State, Reply[]>((state) => forumSelectors.getReplies(state));
-  const users = useSelector<State, ForumUser[]>((state) => forumSelectors.getUsers(state));
-  const login = useSelector<State, string>((state) => userProfileSelector.getLogin(state));
+  const topics = useSelector<State, TopicType[]>(forumSelectors.getTopics);
+  const comments = useSelector<State, Comment[]>(forumSelectors.getComments);
+  const replies = useSelector<State, Reply[]>(forumSelectors.getReplies);
+  const users = useSelector<State, ForumUser[]>(forumSelectors.getUsers);
+  const login = useSelector<State, string>(userProfileSelector.getLogin);
 
   useEffect(() => {
     const userIsExist = users.find((item) => item.name === login);
